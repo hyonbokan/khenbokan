0bbec7583cf1d091aa77941bd7abc2afe2395efd
hyonbokan <mkkanhb@gmail.com>
2024-05-23 15:37:22 +0900

    globe and gradient bg added

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,6 @@
 import { cn } from "@/utils/cn";
+import { BackgroundGradientAnimation } from "./GradientBg";
+import { Globe } from "./GridGlobe";
 
 export const BentoGrid = ({
   className,
@@ -75,16 +77,22 @@ export const BentoGridItem = ({
         </div>
 
         {id === 6 && (
-          // left off here!
+          <BackgroundGradientAnimation>
+            <div className="absolute z-50 flex items-center justify-center text-white font-bold"/>
+          </BackgroundGradientAnimation>
         )}
-      </div>
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+        <div className={ cn(
+          titleClassName, "group-hover/bento:tranlate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+        )}>
+          <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
+            { description }
+          </div>
+          <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
           {title}
+          </div>
         </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-          {description}
-        </div>
+
+        {id == 2 && <Globe />}
       </div>
     </div>
   );
