56b800e855107a85d2e5d512d2974c932c568cdf
hyonbokan <mkkanhb@gmail.com>
2024-05-24 13:33:15 +0900

    globe bg added

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
-import { Globe } from "./GridGlobe";
+import { GlobeDemo } from "./GridGlobe";
 
 export const BentoGrid = ({
   className,
@@ -92,7 +92,7 @@ export const BentoGridItem = ({
           </div>
         </div>
 
-        {id == 2 && <Globe />}
+        {id == 2 && <GlobeDemo />}
       </div>
     </div>
   );
