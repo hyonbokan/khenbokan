900f24a295bf343d6fd1811c376294486f9dd617
hyonbokan <mkkanhb@gmail.com>
2024-06-08 17:26:38 +0900

    minor changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,9 @@ const Footer = () => {
         <div className='flex justify-center items-center md:gap-3 gap-6'>
             {socialMedia.map((profile) => (
                 <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
-                    <img src={profile.img} alt={profile.id} width={20} height={20} />
+                    <a href={profile.link} target="_blank" rel="noopener noreferrer">
+                        <img src={profile.img} alt={profile.id} width={20} height={20} />
+                    </a>
                 </div>
             ))}
         </div>
--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,8 @@
 export const navItems = [
+    { name: "Top", link: "#hero" },
     { name: "About", link: "#about" },
     { name: "Projects", link: "#projects" },
-    { name: "Research", link: "#testimonials" },
+    { name: "Experience", link: "#testimonials" },
     { name: "Contact", link: "#contact" },
   ];
   
@@ -157,31 +158,17 @@ export const navItems = [
   export const workExperience = [
     {
       id: 1,
-      title: "Frontend Engineer Intern",
+      title: "Graduate Researcher",
       desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
       className: "md:col-span-2",
       thumbnail: "/exp1.svg",
     },
     {
       id: 2,
-      title: "Mobile App Dev - JSM Tech",
+      title: "Business Coordinator",
       desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp2.svg",
-    },
-    {
-      id: 3,
-      title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-      className: "md:col-span-2", // change to md:col-span-2
-      thumbnail: "/exp3.svg",
-    },
-    {
-      id: 4,
-      title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
-      className: "md:col-span-2",
-      thumbnail: "/exp4.svg",
     },
   ];
   
@@ -189,6 +176,7 @@ export const navItems = [
     {
       id: 1,
       img: "/git.svg",
+      link: "https://github.com/hyonbokan"
     },
     {
       id: 2,
@@ -197,5 +185,6 @@ export const navItems = [
     {
       id: 3,
       img: "/link.svg",
+      link: "https://www.linkedin.com/in/khen-bo-kan-2909a716b/"
     },
   ];
